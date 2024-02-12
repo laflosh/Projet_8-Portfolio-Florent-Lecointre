763208e80eab50984e8544f23ca70f66c37544af
laflosh <l_florent_l@hotmail.fr>
2024-02-12 15:56:13 +0100

    Intégartion et mis en page header

--- src/_components/Header/_index.jsx
+++ src/_components/Header/_index.jsx
@@ -2,8 +2,38 @@
 
 function Header(){
 
+    return(
 
+        <div>
 
-}
+            <ul className="navbar">
+
+                <li className="navbar__link">
+                    <a href="#presentation">Présentation</a>
+                </li>
+
+                <li className="navbar__link">
+                    <a href="#about">A propos de moi</a>
+                </li>
+
+                <li className="navbar__link">
+                    <a href="#competence">Compétences</a>
+                </li>
+
+                <li className="navbar__link">
+                    <a href="portfolio">Portfolio</a>
+                </li>
+
+                <li className="navbar__link">
+                    <a href="contact">Contact</a>
+                </li>
+
+            </ul>
+
+        </div>
+
+    );
+
+};
 
 export default Header;
