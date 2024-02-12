4a92c58b7a9e397a9bf0f967e68cb8999f161b6f
laflosh <l_florent_l@hotmail.fr>
2024-02-12 18:04:10 +0100

    Intégration et mis en page footer

--- src/_components/Footer/_index.jsx
+++ src/_components/Footer/_index.jsx
@@ -2,8 +2,16 @@
 
 function Footer(){
 
+    return(
 
+        <div className="footer">
 
-}
+            <p className="footer__text">Developed by Florent Lecointre</p>
+
+        </div>
+
+    );
+
+};
 
 export default Footer;
